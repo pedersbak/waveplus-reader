ddba73f59416be6a1ef9ffb155ac7407691e9817
Jan Pedersbæk <pedersbak@gmail.com>
2021-12-12 10:22:08 +0100

    added humidity

--- read_waveplus.py
+++ read_waveplus.py
@@ -243,7 +243,7 @@ try:
                    '"Temperature":"{}",' \
                    '"Pressure":"{}",' \
                    '"CO2 level":"{}",' \
-                   '"VOC level":"{}"}'.format(data[0],data[1],data[2],data[3],data[4],data[5],data[6], data[7   ])
+                   '"VOC level":"{}"}'.format(data[0],data[1],data[2],data[3],data[4],data[5],data[6], data[7])
         if (Mode=='terminal'):
             print tableprint.row(data, width=12)
         elif (Mode=='pipe'):
