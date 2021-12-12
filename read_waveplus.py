b93a8c4095fe9c484fdaf63419cac2dd9c3a7ed9
Jan Pedersbæk <pedersbak@gmail.com>
2021-12-12 10:23:16 +0100

    added humidity

--- read_waveplus.py
+++ read_waveplus.py
@@ -237,7 +237,7 @@ try:
         # Print data
         data = [str(datetime.datetime.now()), humidity, radon_st_avg, radon_lt_avg, temperature, pressure, CO2_lvl, VOC_lvl]
         jsondata = '{"time":"{},' \
-                   '"Humidity":"{}" ' \
+                   '"Humidity":"{}",' \
                    '"Radon ST avg":""{},' \
                    '"Radon LT avg":"{}",' \
                    '"Temperature":"{}",' \
